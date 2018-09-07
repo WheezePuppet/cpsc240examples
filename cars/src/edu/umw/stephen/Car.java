035abd0b7fe3730019195bed7d604e28a9324b8b
Stephen <stephen@umw.edu>
2018-09-06 20:06:10 -0400

    Add 9/6 example.

---
+++ cars/src/edu/umw/stephen/Car.java
@@ -0,0 +1,83 @@
+/*
+ * Sept 6 in-class example: a basic Car class, with constructor and methods
+ * and a main().
+ */
+package edu.umw.stephen;
+
+class Car {
+
+    String make;
+    String model;
+    int yearsOld;
+    int odo;
+    double galsRemaining;
+    double sizeOfTank;
+    double gasMileage;
+
+    Car(String make, String model) {
+        this.make = make;
+        this.model = model;
+        yearsOld = 0;
+        odo = 0;
+        galsRemaining = 0;
+        if (make.equals("Chevy") || make.equals("GM")) {
+            sizeOfTank = 21;
+        } else {
+            sizeOfTank = 13;
+        }
+        if (make.equals("Chevy") && model.equals("Malibu")) {
+            gasMileage = 3;
+        } else {
+            gasMileage = 24;
+        }
+    }
+
+    void fillUp() {
+        this.galsRemaining = this.sizeOfTank;
+    }
+
+    double getTankPerc() {
+        double perc = galsRemaining / sizeOfTank * 100;
+        return perc;
+    }
+
+    void drive(int numMiles) throws Exception {
+        double galsBurned = numMiles / gasMileage;
+        if (galsBurned > galsRemaining) {
+            throw new Exception("Not enough gas!");
+        }
+        galsRemaining = galsRemaining - galsBurned;
+        odo += numMiles;
+    }
+
+    public static void main(String args[]) {
+        System.out.println("Way to go people!");
+        Car minivan = new Car("Chevy","Malibu");
+        minivan.fillUp();
+        try {
+            minivan.drive(10);
+            minivan.drive(500);
+        } catch (Exception e) {
+            System.out.println("Yikes! That drive wasn't possible because:");
+            System.out.println(e);
+        }
+        System.out.println("Your tank is " + 
+            minivan.getTankPerc() + "% full.");
+    }
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
