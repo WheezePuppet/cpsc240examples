d07459ef88046f25dd6ce5db9f136540c3d2c01c
Stephen <stephen@umw.edu>
2018-10-04 19:28:23 -0400

    Complete Holiday Inn example.

--- resort/ReservationSystem.java
+++ resort/ReservationSystem.java
@@ -1,20 +1,23 @@
 
 import java.io.FileReader;
+import java.io.PrintWriter;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class ReservationSystem {
+
     public static void main(String args[]) {
         if (args.length == 0) {
             System.out.println("Usage: java ReservationSystem filename.");
-            System.exit(2);
+            System.exit(1);   // the 1 means nothing. ignore it.
         }
         Scanner s = null;
         try {
             s = new Scanner(new FileReader(args[0]));
         } catch (FileNotFoundException e) {
             System.out.println("No such file " + args[0] + ", dummy!");
-            System.exit(1);
+            System.exit(1);   // the 1 means nothing. ignore it.
         }
 
         String line = s.nextLine();
@@ -22,10 +25,32 @@ public class ReservationSystem {
             line = s.nextLine();
         }
 
-        Resort firstResort = new Resort(s);
-        System.out.println(firstResort);
+        // Let's make an ArrayList of Resort objects, and hydrate them from
+        // the file.
+        ArrayList<Resort> resorts = new ArrayList<Resort>();
 
-        // to be continued...
+        try {
+            while (true) {
+                Resort r = new Resort(s);
+                resorts.add(r);
+            }
+        } catch (NoResortException e) { 
+            // This catch block left intentionally blank!
+        }
+
+        System.out.println("We successfully read " + resorts.size() +
+            " resorts!");
+
+        // Now let's persist them back to a (different) file.
+        try {
+            PrintWriter pw = new PrintWriter("some_data.out");
+            for (Resort r: resorts) {
+                r.store(pw);
+            }
+            pw.close();
+        } catch (Exception e) {
+            System.out.println("We got an error! " + e);
+        }
     }
 
     private static boolean hasOnlyDashes(String x) {
--- resort/Resort.java
+++ resort/Resort.java
@@ -1,18 +1,28 @@
 
 import java.util.Scanner;
+import java.io.PrintWriter;
 
 class Resort {
 
     private String name;
     private String phone;
     private int rating;
+    private int price;
     private String description;
 
-    Resort(Scanner s) {
+    // Hydrate a Resort object from a Scanner that is queued up to point to
+    // the beginning of a resort entry. If no such object can be created
+    // (since we're at the end of the file) throw an Exception instead.
+    Resort(Scanner s) throws NoResortException {
         this.name = s.nextLine();
+        if (this.name.equals("END")) {
+            throw new NoResortException();
+        }
         this.phone = s.nextLine();
         this.rating = Integer.valueOf(s.nextLine());
+        this.price = s.nextLine().length();
 
+        this.description = "";
         String line = s.nextLine();
         while (!line.equals(".")) {
             this.description = this.description + line + "\n";
@@ -20,4 +30,19 @@ class Resort {
         }
     }
 
+    // Persist this Resort object to the PrintWriter passed. Store it in
+    // exactly the same format that we hydrate from.
+    public void store(PrintWriter pw) {
+        pw.println(this.name + "\n" + this.phone + "\n" + this.rating);
+        String dollars = "";
+        for (int i=0; i<price; i++) {
+            dollars += "$";
+        }
+        pw.println(dollars + "\n" + this.description);
+    }
+
+    public String toString() {
+        return this.name + " (" + this.rating + " stars)";
+    }
 }
+
